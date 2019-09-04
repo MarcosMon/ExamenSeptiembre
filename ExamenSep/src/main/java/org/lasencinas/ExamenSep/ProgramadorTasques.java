82275fab609be9ecaa9aefb6f02deebfee261112
marc monjon <sysmarc13@hotmail.com>
2019-09-04 19:20:07 +0200

    Ultima historia de usuario pasada, implementacion de vehiculo en metodo executarTasques

--- ExamenSep/src/main/java/org/lasencinas/ExamenSep/ProgramadorTasques.java
+++ ExamenSep/src/main/java/org/lasencinas/ExamenSep/ProgramadorTasques.java
@@ -4,42 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProgramadorTasques {
-	
+
 	Tasques task = new Tasques();
-	
-	public ProgramadorTasques() {}
-	
-	
+
+	public ProgramadorTasques() {
+	}
+
 	public ProgramadorTasques(Target vehicle) {
-		
+
 		task.setTarget(vehicle);
-		
+
 	}
 
 	public void setTasca(Filtre autenticacio) {
-		
-		
+
 		task.afegirTasca(autenticacio);
-		
+
 	}
-	
+
 	public Tasques getTasques() {
-		
-		
-	
+
 		return task;
 	}
-	
+
 	public void executarTasques(String tasca) {
-		
-		for(Filtre tarea : task.getTasques()) {
-			
+
+		for (Filtre tarea : task.getTasques()) {
+
 			tarea.execucio(tasca);
 		}
 		
-		
-		
+		task.getTarget().execucio(tasca);
+
 	}
-	
 
 }
